26d2f9a3cb14f411ae60bbd90ecbf5dad306007c
Delreya-o <anouk.oms@caramail.fr>
2026-06-25 15:31:30 +0200

    Mini test graphique

---
+++ mylibrary/front/src/components/Bookstore.vue
@@ -0,0 +1,30 @@
+<template>
+    <h3>Contenu renseigné</h3>
+    <ul>
+        <li>author</li>
+        <li>publisher</li>
+        <li>book</li>
+        <li>bookinstance</li>
+    </ul>
+    <form v-on:submit.prevent="search">
+        <input type="text" v-model="userstr" />
+        <button type="submit">Afficher</button>
+    </form>
+    <p>Liste recherchée : {{ userstr }}</p>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userstr: "",
+      result: "",
+    };
+  },
+  methods: {
+    search: function () {
+        this.result = "" + this.userstr;
+    },
+  },
+};
+</script>
+<style></style>
