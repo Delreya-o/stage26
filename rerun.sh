43236eb64239b6731a2e67a6d516d78491b94d5d
Delreya-o <anouk.oms@caramail.fr>
2026-06-29 10:40:37 +0200

    integration de la trad au lancement

--- rerun.sh
+++ rerun.sh
@@ -3,9 +3,18 @@
 # python3 mylibrary/manage.py makemigrations
 # python3 mylibrary/manage.py migrate
 # echo
+
+echo "* * * * * * TRADUCTION * * * * * *"
+echo
+cd ~/Bureau/stage26/mylibrary/shelf
+django-admin makemessages -l=fr_FR -l=en_US
+cd ~/Bureau/stage26/mylibrary/
+./manage.py compilemessages
+
 echo "* * * * * * Re-RUN * * * * * *"
 echo
+cd ~/Bureau/stage26/mylibrary/
 fuser 8000/tcp 2>/dev/null > tmp.txt
 kill -9 $(cat tmp.txt)
 rm tmp.txt
-python3 mylibrary/manage.py runserver
+python3 ./manage.py runserver
